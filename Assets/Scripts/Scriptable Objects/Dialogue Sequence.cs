33797007283a6e20d6a62d89c84357356ca94876
Abdullah Saeed <155902197+UnderlyingReaper@users.noreply.github.com>
2026-05-09 16:40:03 +0500

    Adding dialogue system and stuff

---
+++ Assets/Scripts/Scriptable Objects/Dialogue Sequence.cs
@@ -0,0 +1,8 @@
+﻿// Assets/Scripts/Dialogue/DialogueSequence.cs
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "DialogueSequence", menuName = "LAYOVER/Dialogue Sequence")]
+public class DialogueSequence : ScriptableObject
+{
+    public DialogueLine[] lines;
+}
